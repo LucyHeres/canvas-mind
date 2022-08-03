9de143934414795d91dbd70cbd7e09f6ec850203
liuxin <liuxin@qijiayoudao.com>
2022-08-03 23:26:02 +0800

    feat: 修改配置version

--- version.js
+++ version.js
@@ -1,18 +1,12 @@
-
+const child_process = require("child_process");
 const minimist = require('minimist');
+const execa = require("execa");
 const args = minimist(process.argv.slice(2))
 const issue = args['issue'];
-const exec = require("child_process").exec;
 
-const genVersion = ()=>{
-  const cmd ='npm version patch -m "#'+issue +'"'
-  exec(cmd, (error, stdout, stderr) => {
-    if (!error) {
-      console.log(123)
-    } else {
-      console.log(456)
-    }
-  });
-}
+// const cmd ='npm version patch -m "closes #'+issue +'" && git push && git push --tags'
 
-genVersion()
+(async () => {
+  const { stdout } = await execa("npm", ["version", "patch"]);
+  console.log(stdout);
+})();

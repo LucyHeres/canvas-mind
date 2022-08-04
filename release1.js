4366a5d198fe0244ecbdb1f207a336f5fe7e69e3
liuxin <liuxin@qijiayoudao.com>
2022-08-04 13:38:45 +0800

    feat: 修改配置version

--- release1.js
+++ release1.js
@@ -1,19 +1,29 @@
 const child_process = require("child_process");
 const minimist = require("minimist");
 const execa = require("execa");
+const fs = require("fs");
+
 const args = minimist(process.argv.slice(2));
 const issue = args["issue"];
 
 let commitMessage = "";
-if(issue !== undefined) {
-  commitMessage = "Update to %s , closes #" + issue
-}else{
+if (issue !== undefined) {
+  commitMessage = "Update to %s , closes #" + issue;
+} else {
   commitMessage = "Update to %s";
 }
-// , "-m", ""+commitMessage
+
+const writeChangeLog = () => {
+  fs.readFile("CHANGELOG.md", "utf8", function (err, data) {
+    console.log(data);
+  });
+};
 
 (async () => {
   await execa("npm", ["version", "patch"], { stdio: "inherit" });
   await execa("npm", ["run", "changelog"], { stdio: "inherit" });
+  writeChangeLog()
+  await execa("git", ["add", "CHANGELOG.md"], { stdio: "inherit" });
+  await execa("git", ["commit", "-m", commitMessage], { stdio: "inherit" });
   await execa("git", ["push", "--follow-tags"], { stdio: "inherit" });
 })();

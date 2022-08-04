e408f7e3d0d27fff6b4dc5044afff1644a14b83f
liuxin <liuxin@qijiayoudao.com>
2022-08-04 11:29:10 +0800

    feat: 修改配置version

--- release1.js
+++ release1.js
@@ -5,13 +5,15 @@ const args = minimist(process.argv.slice(2));
 const issue = args["issue"];
 
 let commitMessage = "";
-if(issue !==undefined) {
+if(issue !== undefined) {
   commitMessage = "Update to %s , closes #" + issue
 }else{
   commitMessage = "Update to %s";
 }
+// , "-m", ""+commitMessage
 
 (async () => {
-  await execa("npm", ["version", "patch", "-m", ""+commitMessage], { stdio: "inherit" });
+  await execa("npm", ["version", "patch"], { stdio: "inherit" });
+  await execa("npm", ["run", "changelog"], { stdio: "inherit" });
   await execa("git", ["push", "--follow-tags"], { stdio: "inherit" });
 })();

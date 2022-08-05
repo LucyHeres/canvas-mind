777d34bdae9186b28c015a5c1a74f67ff06ef25b
liuxin <liuxin@qijiayoudao.com>
2022-08-05 15:45:40 +0800

    feat: issues

--- release1.js
+++ release1.js
@@ -7,18 +7,11 @@ const path = require("path");
 const args = minimist(process.argv.slice(2));
 const issueIds = args["issue"]?args["issue"].split(","):[];
 
-let commitMessage = "";
-if (issueIds.length>0) {
-  commitMessage = `Update to %s , closes ` + issueIds.map(issueId=>`#${issueId}`).join(",");
-} else {
-  commitMessage = `Update to %s`;
-}
-console.log(commitMessage);
 const writeChangeLog = () => {
   const changelogPath = path.resolve(__dirname, "CHANGELOG.md");
   let data = fs.readFileSync(changelogPath, "utf8");
   const i = data.indexOf("\n");
-  data = spliceStr(data, i, `\n\n### 关联issue ` + issueIds.map(issueId=>`[#${issueId}](https://github.com/LucyHeres/canvas-mind/issues/${issueId})`).join(","));
+  data = spliceStr(data, i, `\n\n### Issues\n` + issueIds.map(issueId=>`[#${issueId}](https://github.com/LucyHeres/canvas-mind/issues/${issueId})`).join(","));
   fs.writeFileSync(changelogPath, data, "utf8");
 };
 
@@ -26,11 +19,13 @@ const spliceStr = (str, index, newStr) => {
   return str.slice(0, index) + newStr + str.slice(index);
 };
 
-(async () => {
+const main = () => {
   await execa("npm", ["version", "patch"], { stdio: "inherit" });
   await execa("npm", ["run", "changelog"], { stdio: "inherit" });
   writeChangeLog();
   await execa("git", ["add", "CHANGELOG.md"], { stdio: "inherit" });
-  await execa("git", ["commit", "-m", commitMessage], { stdio: "inherit" });
+  await execa("git", ["commit", "-m", `Update to %s`], { stdio: "inherit" });
   await execa("git", ["push", "--follow-tags"], { stdio: "inherit" });
-})();
+}
+
+main();

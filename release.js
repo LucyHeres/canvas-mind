3bab174c0edb4fc589cb25ad8385c556da300006
liuxin <liuxin@qijiayoudao.com>
2022-08-08 10:29:19 +0800

    feat: release脚本

--- release.js
+++ release.js
@@ -1,70 +1,41 @@
-/**
-
-How to do a release:
-
-1. Make sure you have publish access for all packages:
-    - You must be in the CLI team in the npm @vue organization
-    - You must have publish access to vue-cli-version-marker
-    - Make sure you DO NOT have npm per-publish 2-factor / OTP enabled, as it
-      does not work with Lerna (which we use for batch publishing).
-
-2. Run `yarn release`, follow prompts
-
-3A. If everything works properly, the tag should have been auto-pushed and a
-    local changelog commit should have been generated. Go to 4.
-
-3B. If the publish fails half-way, things have gotten hairy. Now you need to
-    go to npm to check which packages have been published and manually publish
-    the ones that have not been published yet. After all have been published:
-
-    3B.1. Push the release git tag to GitHub.
-    3B.2. Run `yarn changelog` to generate changelog commit.
-
-4. Push the changelog commit to `dev` branch.
-
-5. Go to GitHub and verify that the changelog is live.
-
-6. Go to GitHub releases page and publish the release (this is required for
-   the release to show up in the issue helper)
-
-Note: eslint-config-* packages should be released separately & manually.
-
-*/
-
-process.env.VUE_CLI_RELEASE = true;
-
+const child_process = require("child_process");
+const minimist = require("minimist");
 const execa = require("execa");
 const semver = require("semver");
 const inquirer = require("inquirer");
-const minimist = require("minimist");
-// const { syncDeps } = require('./syncDeps')
-// const { buildEditorConfig } = require('./buildEditorConfig')
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const packageJsonData = require("./package.json");
+const step = (msg) => console.log(chalk.cyan(msg));
 
-const cliOptions = minimist(process.argv);
-const curVersion = require("./package.json").version;
+const getPackageVersion = () => packageJsonData.version;
+const writePackageVersion = (newVersion) => {
+  packageJsonData.version = newVersion;
+  fs.writeFileSync("./package.json", JSON.stringify(packageJsonData, "", "\t"));
+};
 
-const release = async () => {
-  console.log(`Current version: ${curVersion}`);
+const curVersion = getPackageVersion();
+const bumps = ["patch", "minor", "major", "prepatch"];
+const versions = {};
+bumps.forEach((b) => {
+  versions[b] = semver.inc(curVersion, b);
+});
+const bumpChoices = bumps.map((b) => ({ name: `${b} (${versions[b]})`, value: b }));
 
-  const bumps = ["patch", "minor", "major", "prerelease"];
-  const versions = {};
-  bumps.forEach((b) => {
-    versions[b] = semver.inc(curVersion, b);
-  });
-  const bumpChoices = bumps.map((b) => ({ name: `${b} (${versions[b]})`, value: b }));
+const spliceStr = (str, index, newStr) => {
+  return str.slice(0, index) + newStr + str.slice(index);
+};
+
+const main = async () => {
+  step(`Current version: ${curVersion}`);
 
   const { bump, customVersion } = await inquirer.prompt([
     {
       name: "bump",
-      message: "Select release type:",
+      message: "请选择要发行的版本：",
       type: "list",
-      choices: [...bumpChoices, { name: "custom", value: "custom" }],
-    },
-    {
-      name: "customVersion",
-      message: "Input version:",
-      type: "input",
-      when: (answers) => answers.bump === "custom",
+      choices: [...bumpChoices],
     },
   ]);
 
@@ -73,46 +44,52 @@ const release = async () => {
   const { yes } = await inquirer.prompt([
     {
       name: "yes",
-      message: `Confirm releasing ${version}?`,
+      message: `确定要发行的版本号是 ${version} 吗?`,
       type: "confirm",
     },
   ]);
 
+  const { issues } = await inquirer.prompt([
+    {
+      name: "issues",
+      message: `请输入需要关联的issue(多个用逗号分隔)：`,
+      type: "input",
+    },
+  ]);
+
+  const issueIds = issues ? issues.split(",") : [];
+
+  const writeChangeLog = () => {
+    const changelogPath = path.resolve(__dirname, "CHANGELOG.md");
+    let data = fs.readFileSync(changelogPath, "utf8");
+    const i = data.indexOf("\n");
+    data = spliceStr(data, i, `\n\n### Issues\n` + issueIds.map((issueId) => `[#${issueId}](https://github.com/LucyHeres/canvas-mind/issues/${issueId})`).join(","));
+    fs.writeFileSync(changelogPath, data, "utf8");
+  };
+
   if (yes) {
-    // await syncDeps({
-    //   version,
-    //   local: true,
-    //   skipPrompt: true
-    // })
-    delete process.env.PREFIX;
-
-    // await buildEditorConfig()
-
     try {
-      await execa("git", ["add", "-A"], { stdio: "inherit" });
-      await execa("git", ["commit", "-m", "chore: pre release sync"], { stdio: "inherit" });
+      step("\nUpdating package version...");
+      // await execa("npm", ["version", version], { stdio: "inherit" });
+      writePackageVersion(version);
+
+      // step("\nGenerating changelog...");
+      // await execa("npm", ["run", "changelog"], { stdio: "inherit" });
+      // writeChangeLog();
+      // await execa("git", ["add", "CHANGELOG.md"], { stdio: "inherit" });
+      // await execa("git", ["commit", "-m", `Update to v${version}`], { stdio: "inherit" });
+
+      // step("\nPushing ...");
+      // await execa("git", ["push", "--follow-tags"], { stdio: "inherit" });
+
+      // step("\nDone ...");
     } catch (e) {
-      // if it's a patch release, there may be no local deps to sync
+      //
     }
   }
-
-  let distTag = cliOptions["dist-tag"] || "latest";
-  if (bump === "prerelease" || semver.prerelease(version)) {
-    distTag = "next";
-  }
-
-  const lernaArgs = ["publish", version, "--dist-tag", distTag];
-  // keep all packages' versions in sync
-  lernaArgs.push("--force-publish");
-
-  if (cliOptions["local-registry"]) {
-    lernaArgs.push("--no-git-tag-version", "--no-commit-hooks", "--no-push", "--yes");
-  }
-
-  await execa(require.resolve("lerna/cli"), lernaArgs, { stdio: "inherit" });
 };
 
-release().catch((err) => {
+main().catch((err) => {
   console.error(err);
   process.exit(1);
 });

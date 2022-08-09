c6024944c3a349b4e1f6e1d569e548bce36ccd33
liuxin <liuxin@qijiayoudao.com>
2022-08-09 10:47:38 +0800

    chore: update to v1.6.7

---
+++ scripts/release.js
@@ -0,0 +1,96 @@
+const minimist = require("minimist");
+const execa = require("execa");
+const semver = require("semver");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const regex = /tag:\s*(.+?)[,)]/;
+const step = (msg) => console.log(chalk.cyan(msg));
+
+// 获取参数中的版本类型
+const argv = minimist(process.argv.slice(2));
+const bump = argv['version'] || "patch";
+
+// 获取package.json中的version
+const packageJsonData = require("../package.json");
+const getPackageVersion = () => packageJsonData.version;
+
+// 生成目标版本号
+const curVersion = getPackageVersion();
+const targetVersion = semver.inc(curVersion, bump);
+
+// 修改package.json中的version
+const writePackageVersion = (newVersion) => {
+  packageJsonData.version = newVersion;
+  const pkgPath = path.resolve(__dirname, '../package.json')
+  fs.writeFileSync(pkgPath, JSON.stringify(packageJsonData, null, 2));
+};
+
+const spliceStr = (str, index, newStr) => {
+  return str.slice(0, index) + newStr + str.slice(index);
+};
+
+// 在changelog中写入issue相关内容
+const writeChangeLog = (version) => {
+  const issueIds = getIssueIds();
+  // const issueIds = issues ? issues.split(",") : [];
+  if (issueIds && issueIds.length > 0) {
+    const changelogPath = path.resolve(__dirname, "../CHANGELOG.md");
+    let data = fs.readFileSync(changelogPath, "utf8");
+    const i = data.indexOf("\n");
+    if(!data.slice(0,i).includes(`[${version}]`)){
+      return;
+    }
+    data = spliceStr(data, i, `\n\n### Issues\n` + issueIds.map((issueId) => `[#${issueId}](https://github.com/LucyHeres/canvas-mind/issues/${issueId})`).join(","));
+    fs.writeFileSync(changelogPath, data, "utf8");
+  }
+};
+
+// 从git log中获取issues
+const getIssueIds = async () => {
+  try {
+    const { stdout } = await execa("git", ["log", "--decorate", "--no-color"], { maxBuffer: Infinity });
+    const tags = stdout.match(regex)
+    const str = stdout.slice(0,tags.index)
+    const issues = str.match(/\s+Closes #\d+\\n/ig) || [];
+    const issueIds = issues.map(issue => issue.replace(/[^\d]/ig,""));
+    return issueIds;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+
+const main = async () => {
+  step(`Current version: ${curVersion} , Target version: ${targetVersion}`);
+
+  try {
+    step("\nUpdating package version...");
+    writePackageVersion(targetVersion);
+
+    step("\nGenerating changelog...");
+    await execa("npm", ["run", "changelog"], { stdio: "inherit" });
+    await execa("git", ["add", "-A"], { stdio: "inherit" });
+    await execa("git", ["commit", "-m", `chore: update to v${targetVersion}`], { stdio: "inherit" });
+
+    writeChangeLog(targetVersion);
+    await execa("git", ["add", "-A"], { stdio: "inherit" });
+    await execa("git", ["commit", "-m", `chore: update v${targetVersion}-changelog`], { stdio: "inherit" });
+
+    step("\nPushing ...");
+    await execa("git", ["tag", "v" + targetVersion], { stdio: "inherit" });
+    await execa("git", ["push"], { stdio: "inherit" });
+    await execa("git", ["push", "--tags"], { stdio: "inherit" });
+
+    step("\nSuccess 版本发布成功!");
+  } catch (e) {
+    step("\noh no 版本发布失败了！");
+    console.error(e);
+  }
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
